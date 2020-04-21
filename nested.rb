0739f5c0ba4982612e1db88518a886eef0c74731
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-21 01:24:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 return programmer_hash[:dennis_ritchie][0]
+		 return programmer_hash[:dennis_ritchie][4]
 end
 
 def adding_matz
