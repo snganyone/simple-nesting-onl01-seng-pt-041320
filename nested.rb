cd7bc19e47cc92579bede36dc35cbf33a90074de
Shishiri Nganyone <snganyone1@gmail.com>
2020-04-21 02:58:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -94,7 +94,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "gui",
+          :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
@@ -104,7 +104,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
 
 end
 
